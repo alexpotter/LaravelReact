10e05b6190144a6de7e60fc1b873002830c99e17
Alex Potter <alex.potter1993@gmail.com>
2018-10-04 15:44:48 +0100

    React router - to be tested

--- client/app/App.js
+++ client/app/App.js
@@ -1,10 +1,25 @@
 //@flow
 import * as React from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import Home from 'app/Home'
+import Contact from 'app/Contact'
 
-class App extends React.Component {
-    render() {
-        return <div>Shining Stars</div>
-    }
-}
+const App = () => (
+    <Router>
+        <div>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/contact">Contact</Link>
+                </li>
+            </ul>
+
+            <Route exact path="/" component={Home} />
+            <Route exact path="/contact" component={Contact} />
+        </div>
+    </Router>
+)
 
 export default App
---
+++ client/app/Contact/index.js
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+const Contact = () => (
+    <div>Contact</div>
+)
+
+export default Contact
