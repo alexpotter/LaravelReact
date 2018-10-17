264c83b2d40f4a7246414d4975e384a5986719f1
Alex Potter <alex.potter1993@gmail.com>
2018-10-17 16:11:51 +0100

    Flow

--- client/admin/App.js
+++ client/admin/App.js
@@ -1,10 +1,6 @@
-//@flow
+// @flow
 import * as React from 'react'
 
-class App extends React.Component {
-	render() {
-		return <div>Shining Stars Admin</div>
-	}
-}
+const App = () => <div>Shining Stars Admin</div>
 
 export default App
